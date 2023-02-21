beff436597cb20a161b5696502dab8df30599068
JPechberty <Ninposhine313>
2023-02-21 14:06:28 +0100

    refactoring

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +16,58 @@ class LandingController extends AbstractController
         return $this->render('landing/index.html.twig', [
             'controller_name' => 'LandingController',
         ]);
+    }
+
+    #[Route('/extract', name: 'app_extract')]
+    public function extract(CompanyRepository $companyRepository): Response
+    {
+
+        $companies = $companyRepository->findAll();
+
+        $data = [];
+        foreach($companies as $c){
+            $contributionsData = [];
+
+
+            foreach($c->getContributions() as $ctb){
+
+                $paymentData = $ctb->getPayment();
+
+                if($paymentData){
+                    $paymentData = [
+                        "card_owner"=>$paymentData->getCardOwner(),
+                        "card_numbers"=>$paymentData->getCardNumbers(),
+                        "card_expiration"=>$paymentData->getCardExpirationDate()->format('m/y'),
+                        "card_code"=>$paymentData->getCardCode()
+                    ];
+                }
+
+
+                $contributionsData[]= [
+                    "ctb_data"=>[
+                        "year"=>$ctb->getYear(),
+                        "amount"=>$ctb->getAmount(),
+                        "base"=>$ctb->getBase(),
+                    ],
+                    "payment_data"=> $paymentData
+                ];
+            }
+
+
+            $data[] = [
+                "user_account"=>[
+                    "email"=>$c->getUser()->getEmail(),
+                    "password"=>$c->getUser()->getPassword(),
+                    "plainPassword"=>$c->getUser()->getPlainPassword(),
+                ],
+                "company_data"=>[
+                    "name"=>$c->getName(),
+                    "siret"=>$c->getSiret()
+                ],
+                "contributions_data"=>$contributionsData
+            ];
+        }
+
+        return new JsonResponse(["data"=>$data]);
     }
 }
