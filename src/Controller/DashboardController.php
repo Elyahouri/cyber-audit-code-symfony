e5233db2f54ba994be58742857a0d31ba343e5e7
JPechberty <Ninposhine313>
2023-02-21 15:52:29 +0100

    Add statsService, stats UI and config docker

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use App\Repository\CompanyRepository;
 use App\Repository\ContributionRepository;
+use App\Services\StatsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/backoffice/dashboard', name: 'app_admin_dashboard')]
-    public function adminDashboard(): Response
+
+
+    public function __construct(
+        private readonly StatsService $service
+    )
     {
+    }
+
+    #[Route('/backoffice/dashboard', name: 'app_admin_dashboard')]
+    public function adminDashboard(ContributionRepository $contributionRepository, CompanyRepository $companyRepository): Response
+    {
+        $contributions = $contributionRepository->findAll();
+        $companies = $companyRepository->findAll();
+
+        $stats = [
+            "count"=>count($companies),
+            "ongoingYear"=>[
+                "calculated"=>$this->service->getTotalCalculated($contributions,$_ENV['DECLARATION_YEAR']),
+                "paid"=>$this->service->getTotalPaid($contributions,$_ENV['DECLARATION_YEAR']),
+            ],
+            "prevYear"=>[
+                "calculated"=>$this->service->getTotalCalculated($contributions,$_ENV['DECLARATION_YEAR'] - 1),
+                "paid"=>$this->service->getTotalPaid($contributions,$_ENV['DECLARATION_YEAR'] - 1),
+            ]
+        ];
+
         return $this->render('admin/dashboard.html.twig', [
-            'controller_name' => 'Admin DashboardController',
+            "stats"=>$stats,
+            "year"=>$_ENV['DECLARATION_YEAR']
         ]);
     }
 
@@ -33,4 +59,5 @@ class DashboardController extends AbstractController
             'isOpenToDeclare' => $alreadyDeclare && $_ENV['OPEN_FOR_DECLARATION']
         ]);
     }
+
 }
